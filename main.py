13b58529cb6ba099d8117ac6540162f279ec5710
Skinde <migue.lama@yahoo.com>
2019-05-06 09:53:37 -0500

    Añadido main

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import sys, pygame
+pygame.init()
 
+size = width, height = 320, 240 
+speed = [2,2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size) #Clase ventana
+
+while 1:  #Loop principal 
+	for event in pygame.event.get(): #Evento cerrar 
+		if event.type == pygame.QUIT: sys.exit()
+	screen.fill(black)
+	pygame.display.flip()
