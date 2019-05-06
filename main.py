e094601587232b9796431f576fe30152e82d3407
jeanpaulHT <50341023+jeanpaulHT@users.noreply.github.com>
2019-05-06 15:31:48 -0500

    hola

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size) #Clase ventana
 
 while 1:  #Loop principal 
-	for event in pygame.event.get(): #Evento
+	for event in pygame.event.get(): #Evento test
 		if event.type == pygame.QUIT: sys.exit()
 	screen.fill(black)
 	pygame.display.flip()
 	#hola
+	#EFICIENCIAygygy
