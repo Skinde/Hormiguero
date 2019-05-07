3775746b638c9a132008fbb588766e8bb8f777b5
Dio12334 <50332452+Dio12334@users.noreply.github.com>
2019-05-07 15:28:35 -0500

    hola

--- Inicio.py
+++ Inicio.py
@@ -4,7 +4,7 @@ Logo = cargar("logo.png")
 pygame.display.set_caption("Hormiguero")
 pygame.display.set_icon(Icono)
 pantalla.fill(blanco)
-
+hormiga_reina = cargar("hormiga_reina.png")
 for n in range(450):
 	for x in range(800):
 		mapa[n][x] = 1
--- Renderizacion.py
+++ Renderizacion.py
@@ -4,10 +4,12 @@ def renderizacion():
 		pygame.display.flip()
 		time.sleep(2)
 		pantalla.fill(blanco)
-	if fase_dejuego == 1: 
-		for n in range(450):
-			for x in range(800):
-				if mapa[n][x] == 1:
-					dibujar_rectangulo((78,59,49),x,n+50,1,1)
+	for n in range(450):
+		for x in range(800):
+			if mapa[n][x] == 1:
+				dibujar_rectangulo((78,59,49),x,n+50,1,1)
+	dibujar(hormiga_reina,400, 42)
 
 	pygame.display.flip() #Actualiza la pantalla
+
+	
