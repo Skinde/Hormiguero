ff51b0dba67eda28c6544fddc061188cdbfcd26f
Skinde <migue.lama@yahoo.com>
2019-05-06 23:09:45 -0500

    Titulo y otros

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 #Librerias
-import sys, pygame
+import sys, pygame, os
 
 #Variables
 ratio = width, height = 800, 500 
 fondo = 255,255,255
-
+game_phase = 0
 #Clases
 pygame.init()
-pantalla = pygame.display.set_mode(ratio) 
+pantalla = pygame.display.set_mode(ratio)
+archivo = str(os.getcwd())
+
+#Imagenes y Sprites
+Icono = pygame.image.load(archivo+"\icono.png").convert_alpha()
 
 #Funcion del juego
 def juego(): 
 	hola = 1
 
 #Funcion de sprites
-def renderizacion(): 
+def renderizacion():
 	pygame.display.flip()
 
-
+#Ordenes de entrada
+pygame.display.set_caption("Hormiguero")
+pygame.display.set_icon(Icono)
 pantalla.fill(fondo)
 
 #Bucle principal
