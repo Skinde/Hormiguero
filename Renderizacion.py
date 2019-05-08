0af3fb904a02392f25f3429c477f25facca73366
Skinde <migue.lama@yahoo.com>
2019-05-07 23:27:20 -0500

    Ordenado

--- Renderizacion.py
+++ Renderizacion.py
@@ -1,17 +1,23 @@
 def renderizacion():  # PONGAN SU CODIGO AQUI ESTO SE EJECUTA EN UN BUCLE
-	if fase_dejuego == 0: #Logo de inicio
+
+	#Logo de inicio
+	if fase_dejuego == 0: 
 		dibujar(Utec, 285, 140)
 		pygame.display.flip()   
 		time.sleep(2)
 	pantalla.fill((81, 209, 246))
 
-	for n in range(450): #Dibujo de el mapa
+	#Dibujo de el mapa
+	for n in range(450): 
 		for x in range(800):
 			if mapa[n][x] == 1:
 				dibujar_rectangulo((78,59,49),x,n+50,1,1)
 
-	dibujar(hormiga_reina,hormigax,hormigay) #Dibujo de la hormiga reina
-	pygame.display.flip() #Actualizacion del codigo
+	#Dibujo de la hormiga reina
+	dibujar(hormiga_reina,hormigax,hormigay) 
+
+	#Actualizacion de la ventana
+	pygame.display.flip() 
 	pantalla.fill((81, 209, 246))
 
 	
